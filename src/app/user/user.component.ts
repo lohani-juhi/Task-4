e0c4140568fc8d30fcf8ddc9d1b0260e9103df28
juhi <juhilohani1910@gmail.com>
2024-06-19 12:53:02 +0530

    Committed

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component,EventEmitter,Input, Output} from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Output() select=new EventEmitter();
+  @Input({required:true})user!:User;
+  @Input({required:true}) selected!:boolean;
+  get imagePath() {
+    return 'assets/users/'+ this.user.avatar;
+   }
+
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }
+}
